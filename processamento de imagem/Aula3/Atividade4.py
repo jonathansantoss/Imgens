bc6cfffaea7f06c7a528f8a4a64be065d6c750f5
jonathansantoss <jonathan.gomess1@gmail.com>
2018-09-26 00:44:01 -0300

    Ajustes

--- processamento de imagem/Aula3/Atividade4.py
+++ processamento de imagem/Aula3/Atividade4.py
@@ -2,17 +2,15 @@ import matplotlib.pyplot as pl
 from PIL import Image
 
 imglenna = Image.open('Lenna.png')
+imglennaCinza = imglenna.convert('L')
+imglennaCinza.show()
+
 img = Image.new('L', (imglenna.size[0] , imglenna.size[1]), 'BLACK')
 
 w = imglenna.size[0]
 h = imglenna.size[1]
 
 histograma = [0] * 256
-
-def intensidade(tupla = ()):
-    r,g,b = tupla
-    m = int( (r+g+b) / 3)
-    return m
 
 def maiorElemento(lista = []):
     elementoMaior = 0
@@ -32,9 +30,27 @@ def menorElemento(lista = ()):
             elementoMenor = i
     return elementoMenor
 
+def maiorMatriz():
+    elementoMaior = 0
+    for i in range(h):
+        for j in range(w):
+            X = imglennaCinza.getpixel( (i,j) )
+            if X > elementoMaior:
+                elementoMaior = X
+    return elementoMaior 
+
+def menorMatriz():
+    elementoMenor = imglennaCinza.getpixel((0,0))
+    for i in range(h):
+        for j in range(w):
+            X = imglennaCinza.getpixel( (i,j) )
+            if X < elementoMenor:
+                elementoMenor = X
+    return elementoMenor
+
 for linha  in range(h):
     for coluna in  range(w):
-        X = intensidade(imglenna.getpixel((linha, coluna)))
+        X = imglennaCinza.getpixel((linha, coluna))
         histograma[X] += 1
         
 pl.plot(histograma)
@@ -42,3 +58,15 @@ pl.show()
 
 Gmaior = maiorElemento(histograma)
 Gmenor = menorElemento(histograma)
+            
+Fmaior = maiorMatriz()
+Fmenor = menorMatriz()
+
+for height in range(h):
+    for width in range(w):
+        X = imglennaCinza.getpixel((height, width))
+        G = int( (Gmaior - Gmenor) / (Fmaior - Fmenor) * (X - Fmenor) + Gmenor )
+        img.putpixel((height,width), G)
+        
+        
+img.show()
