71b14ffd0950efb38f628175e72d665bc2210570
jonathansantoss <jonathan.gomess1@gmail.com>
2018-09-27 08:59:19 -0300

    ajustes

--- processamento de imagem/Aula3/Atividade6/Atividade_6_1.py
+++ processamento de imagem/Aula3/Atividade6/Atividade_6_1.py
@@ -5,14 +5,24 @@ imglenna = Image.open('Lenna.png')
 imgLennaCinza = imglenna.convert('L')
 imgLennaCinza.show()
 
+
 img = Image.new('L', (1000, 1000) , 'BLACK')
+img2 = Image.new('L', (imgLennaCinza.size[0], imgLennaCinza.size[1]) , 'BLACK')
 
 h = imgLennaCinza.size[0]
 w = imgLennaCinza.size[1]   
+
+for linha in range(h):
+    for coluna in range(w):
+        r,g,b = imglenna.getpixel((linha, coluna))
+        pixel = int((r+g+b)/3)
+        img2.putpixel((int(coluna/2) , int(linha/2)), pixel)
+        
+img2.show()
         
 for linha in range(h):
     for coluna in range(w):
-        pixel = imgLennaCinza.getpixel((linha, coluna))
+        pixel = img2.getpixel((linha, coluna))
         Y = int(coluna * math.sin(45) + linha * math.cos(45))
         X = int((coluna * math.cos(45) - linha * math.sin(45)))
         if X < 0:
