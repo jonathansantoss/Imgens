ef2531a18b472080c0258ede7529478cd1fa44fc
jonathansantoss <jonathan.gomess1@gmail.com>
2018-09-27 10:40:24 -0300

    AJUSTES

--- processamento de imagem/Aula3/Atividade6/Atividade_6_1.py
+++ processamento de imagem/Aula3/Atividade6/Atividade_6_1.py
@@ -5,7 +5,6 @@ imglenna = Image.open('Lenna.png')
 imgLennaCinza = imglenna.convert('L')
 imgLennaCinza.show()
 
-
 img = Image.new('L', (1000, 1000) , 'BLACK')
 img2 = Image.new('L', (imgLennaCinza.size[0], imgLennaCinza.size[1]) , 'BLACK')
 
@@ -14,20 +13,19 @@ w = imgLennaCinza.size[1]
 
 for linha in range(h):
     for coluna in range(w):
-        r,g,b = imglenna.getpixel((linha, coluna))
-        pixel = int((r+g+b)/3)
-        img2.putpixel((int(coluna/2) , int(linha/2)), pixel)
+        pixel = imgLennaCinza.getpixel((coluna, linha))
+        img2.putpixel((int(coluna / 4) + 300 , int(linha / 4)+300), pixel)
         
 img2.show()
         
 for linha in range(h):
     for coluna in range(w):
-        pixel = img2.getpixel((linha, coluna))
+        pixel = img2.getpixel((coluna, linha))
         Y = int(coluna * math.sin(45) + linha * math.cos(45))
         X = int((coluna * math.cos(45) - linha * math.sin(45)))
         if X < 0:
-            X = X *(-1)
-        img.putpixel((int(X/2) + 100, int(Y/2) + 100), pixel)
+            X = X * (-1)
+        img.putpixel((int(X / 2) + 100, int(Y / 2) + 100), pixel)
         
 img.show()
 img.save('img1.png')
