77fa328a2688833486e5485caf4bbfa904518750
jonathansantoss <jonathan.gomess1@gmail.com>
2018-09-30 16:09:06 -0300

    Ajustes

--- processamento de imagem/Aula3/Atividade6/atividade_6_4.py
+++ processamento de imagem/Aula3/Atividade6/atividade_6_4.py
@@ -6,10 +6,9 @@ imagemLennaCinza = imagemLenna.convert('L')
 
 h , w = imagemLennaCinza.size
 
-for i in range(int((h * 0.25) )):
-    for j in range(int((w * 0.25)) ):
-        imagemLennaCinza.putpixel( ( randrange(h), randrange(w) ) , 255)
-
+for i in range(int((h * 0.25))):
+    for j in range(int((w * 0.25))):
+        imagemLennaCinza.putpixel((randrange(h), randrange(w)) , 255)
 
 imagemLennaCinza.show()  
 imagemLennaCinza.save('img4.png')  
