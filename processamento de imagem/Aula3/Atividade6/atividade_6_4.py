da8ccd8d5f0c665c07b7d7ecb7b1dbf7d0329ff7
jonathansantoss <jonathan.gomess1@gmail.com>
2018-09-30 12:34:52 -0300

    Ajustes

---
+++ processamento de imagem/Aula3/Atividade6/atividade_6_4.py
@@ -0,0 +1,15 @@
+from PIL import Image
+from random import randrange
+
+imagemLenna = Image.open('Lenna.png')
+imagemLennaCinza = imagemLenna.convert('L')
+
+h , w = imagemLennaCinza.size
+
+for i in range(int((h * 0.25) )):
+    for j in range(int((w * 0.25)) ):
+        imagemLennaCinza.putpixel( ( randrange(h), randrange(w) ) , 255)
+
+
+imagemLennaCinza.show()  
+imagemLennaCinza.save('img4.png')  
