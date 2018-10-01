0238e182d5c8eefeea8af3952ea32b1a9686e945
jonathansantoss <jonathan.gomess1@gmail.com>
2018-09-30 23:52:38 -0300

    Ajustes

--- processamento de imagem/Aula3/Atividade6/Atividade_6_8.py
+++ processamento de imagem/Aula3/Atividade6/Atividade_6_8.py
@@ -1,5 +1,49 @@
-'''
-Created on 30 de set de 2018
+from PIL import Image
 
-@author: jonathan
-'''
+imagemLenna = Image.open('Lenna.png')
+imagemLennaCinza = imagemLenna.convert('L')
+imagemLennaCinza.show()
+
+img = Image.new('L', (imagemLennaCinza.size[0], imagemLennaCinza.size[1]), 'BLACK')
+
+h , w = imagemLennaCinza.size
+
+histograma = [0] * 256
+
+
+def Maior():
+    valorMaior = 0
+    m = 0
+    for i in range(len(histograma)):
+        if histograma[i] > valorMaior:
+            valorMaior = histograma[i]
+            m = i
+            
+    return m
+
+
+def Menor():
+    valorMenor = histograma[0]
+    m = 0
+    for i in range(len(histograma)):
+        if histograma[i] < valorMenor:
+            valorMenor = histograma[i]
+            m = i
+            
+    return m
+
+
+for i in range(h):
+    for j in range(w):
+        pixel = imagemLennaCinza.getpixel((i, j))
+        histograma[int(pixel)] += 1
+        
+for i in range(h):
+    for j in range(w):
+        maior = Maior()
+        menor = Menor()
+        pixel = (imagemLennaCinza.getpixel((i, j)) - maior) * ((128) / (maior - menor)) + 128
+        img.putpixel((i , j), int(pixel)) 
+
+img.show()
+img.save('img8.png')
