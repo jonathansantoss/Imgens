6443477a1e1a4e1eaf98f54954d5e61305074afa
jonathansantoss <jonathan.gomess1@gmail.com>
2018-10-02 10:36:18 -0300

    ajustes

--- processamento de imagem/Aula3/Atividade5.py
+++ processamento de imagem/Aula3/Atividade5.py
@@ -1,13 +1,13 @@
 from PIL import Image
 
-imglenna = Image.open('img.jpg')
+imglenna = Image.open('images.jpg')
 imglennaCinza = imglenna.convert('L')
 imglennaCinza.show()
 
-imgPassaFaixa = Image.new('L', (imglennaCinza.size[0], imglennaCinza.size[1]), 'BLACK')
-imgPassaAlta = Image.new('L', (imglennaCinza.size[0], imglennaCinza.size[1]), 'BLACK')
-imgPassaBaixa = Image.new('L', (imglennaCinza.size[0], imglennaCinza.size[1]), 'BLACK')
-img = Image.new('L', (imglennaCinza.size[0] + 2, imglennaCinza.size[1] + 2), 'BLACK')
+imgMascara1 = Image.new('L', (imglennaCinza.size[0], imglennaCinza.size[1]), 'BLACK')
+imgMascara2 = Image.new('L', (imglennaCinza.size[0], imglennaCinza.size[1]), 'BLACK')
+imgMascara3 = Image.new('L', (imglennaCinza.size[0], imglennaCinza.size[1]), 'BLACK')
+img = Image.new('L', (imglennaCinza.size[0] + 4, imglennaCinza.size[1] + 4), 'BLACK')
 
 h = imglennaCinza.size[0]
 w = imglennaCinza.size[1]
@@ -17,60 +17,64 @@ for i in range(h):
         X = imglennaCinza.getpixel((i, j))
         img.putpixel((i + 1, j + 1), X)
 
-mascaraBaixa = ([1,1,1],[1,1,1],[1,1,1])
-mascaraAlta = ([-1, -1, -1], [0, 0, 0], [1, 1, 1])
-mascaraFaixa = ([0, -1, 0], [-1, 5, -1], [0, -1, 0])
 
-
-def PassaAlto(height, width):
-    T = img.getpixel((height, width)) * mascaraAlta[1][1]
-    X1 = img.getpixel((height + 1, width + 1)) * mascaraAlta[2][2]
-    X2 = img.getpixel((height - 1, width + 1)) * mascaraAlta[0][0]
-    X3 = img.getpixel((height + 1, width)) * mascaraAlta[2][1]
-    X4 = img.getpixel((height - 1, width)) * mascaraAlta[0][1]
-    X5 = img.getpixel((height, width + 1)) * mascaraAlta[1][2]
-    X6 = img.getpixel((height, width - 1)) * mascaraAlta[1][0]
-    X7 = img.getpixel((height - 1, width - 1)) * mascaraAlta[0][0]
-    X8 = img.getpixel((height + 1, width - 1)) * mascaraAlta[2][0]
-    M = int((T + X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8))
+def Mascara1(height, width):
+    T = img.getpixel((height, width)) 
+    X3 = img.getpixel((height + 1, width)) 
+    X4 = img.getpixel((height - 1, width)) 
+    X5 = img.getpixel((height, width + 1)) 
+    X6 = img.getpixel((height, width - 1))  
+    M = int((T + X3 + X4 + X5 + X6) / 5)
     return M
 
-def PassaBaixa(height, width):
-    T = img.getpixel((height, width)) * mascaraBaixa[1][1]
-    X1 = img.getpixel((height + 1, width + 1)) * mascaraBaixa[2][2]
-    X2 = img.getpixel((height - 1, width + 1)) * mascaraBaixa[0][0]
-    X3 = img.getpixel((height + 1, width)) * mascaraBaixa[2][1]
-    X4 = img.getpixel((height - 1, width)) * mascaraBaixa[0][1]
-    X5 = img.getpixel((height, width + 1)) * mascaraBaixa[1][2]
-    X6 = img.getpixel((height, width - 1)) * mascaraBaixa[1][0]
-    X7 = img.getpixel((height - 1, width - 1)) * mascaraBaixa[0][0]
-    X8 = img.getpixel((height + 1, width - 1)) * mascaraBaixa[2][0]
-    M = int((T + X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8))
+
+def Mascara2(height, width):
+    T = img.getpixel((height, width)) 
+    X1 = img.getpixel((height + 1, width + 1)) 
+    X2 = img.getpixel((height - 1, width + 1)) 
+    X3 = img.getpixel((height + 1, width)) 
+    X4 = img.getpixel((height - 1, width)) 
+    X5 = img.getpixel((height, width + 1)) 
+    X6 = img.getpixel((height, width - 1)) 
+    X7 = img.getpixel((height - 1, width - 1)) 
+    X8 = img.getpixel((height + 1, width - 1)) 
+    M = int((T + X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8) / 9)
     return M
 
-def PassaFaixa(height, width):
-    T = img.getpixel((height, width)) * mascaraFaixa[1][1]
-    X1 = img.getpixel((height + 1, width + 1)) * mascaraFaixa[2][2]
-    X2 = img.getpixel((height - 1, width + 1)) * mascaraFaixa[0][0]
-    X3 = img.getpixel((height + 1, width)) * mascaraFaixa[2][1]
-    X4 = img.getpixel((height - 1, width)) * mascaraFaixa[0][1]
-    X5 = img.getpixel((height, width + 1)) * mascaraFaixa[1][2]
-    X6 = img.getpixel((height, width - 1)) * mascaraFaixa[1][0]
-    X7 = img.getpixel((height - 1, width - 1)) * mascaraFaixa[0][0]
-    X8 = img.getpixel((height + 1, width - 1)) * mascaraFaixa[2][0]
-    M = int((T + X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8))
+
+def Mascara3(height, width):
+    T = img.getpixel((height, width)) 
+    X1 = img.getpixel((height + 1, width + 1)) 
+    X2 = img.getpixel((height - 1, width + 1)) 
+    X3 = img.getpixel((height + 1, width)) 
+    X4 = img.getpixel((height - 1, width)) 
+    X5 = img.getpixel((height, width + 1)) 
+    X6 = img.getpixel((height, width - 1)) 
+    X7 = img.getpixel((height - 1, width - 1)) 
+    X8 = img.getpixel((height + 1, width - 1)) 
+    
+    X9 = img.getpixel((height + 2, width + 2)) 
+    X10 = img.getpixel((height - 2, width + 2)) 
+    X11 = img.getpixel((height + 2, width)) 
+    X12 = img.getpixel((height - 2, width)) 
+    X13 = img.getpixel((height, width + 2)) 
+    X14 = img.getpixel((height, width - 2)) 
+    X15 = img.getpixel((height - 2, width - 2)) 
+    X16 = img.getpixel((height + 2, width - 2))
+    
+    M = int((T + X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8 + X9 + X10 + X11 + X12 + X13 + X14 + X15 + X16) / 17)
     return M
 
     
-for height in range(1, h + 1):
-    for width in range(1, w + 1):
-        mAlta =PassaAlto(height, width)
-        mBaixa = PassaBaixa(height, width)
-        mFaixa = PassaFaixa(height, width)
-        imgPassaAlta.putpixel((height - 1, width - 1), mAlta)
-        imgPassaBaixa.putpixel((height - 1, width - 1), mBaixa)
-        imgPassaFaixa.putpixel((height - 1, width - 1), mFaixa)
+for height in range(2, h + 2):
+    for width in range(2, w + 2):
+        m1 = Mascara1(height, width)
+        m2 = Mascara2(height, width)
+        m3 = Mascara3(height, width)
+        imgMascara1.putpixel((height - 2, width - 2), m1)
+        imgMascara2.putpixel((height - 2, width - 2), m2)
+        imgMascara3.putpixel((height - 2, width - 2), m3)
        
-imgPassaAlta.show()
-imgPassaBaixa.show()
-imgPassaFaixa.show()
+imgMascara1.show()
+imgMascara2.show()
+imgMascara3.show()
